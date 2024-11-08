55e7c7f85c6266e12cc57dc3f4b12e39cdddb07d
bagusyanuar <50134851+bagusyanuar@users.noreply.github.com>
2024-11-08 15:40:05 +0700

    style(component): sidebar scroll

--- resources/views/components/navigation/sidebar/wrapper.blade.php
+++ resources/views/components/navigation/sidebar/wrapper.blade.php
@@ -1,90 +1,23 @@
-<div class="fixed top-0 left-0 h-screen shadow-sidebar w-sidebar px-3">
+<div class="fixed top-0 left-0 h-screen shadow-sidebar w-sidebar flex flex-col">
     <!-- sidebar header -->
-    <div class="w-full h-20 flex justify-center items-center border-b border-neutral-300 mb-3">
-        <img
-            src="{{ asset('/assets/images/logo.png') }}"
-            alt="img-logo"
-            class="h-12">
-    </div>
-    <!-- sidebar menu -->
-    <div class="w-full flex flex-col">
-        <p class="text-xs text-neutral-400 font-light mb-1">Menu</p>
-        <div class="flex flex-col w-full gap-1">
-            <x-navigation.sidebar.item
-                to="/dashboard"
-                icon="house"
-                title="Dashboard"
-                active="{{ \Illuminate\Support\Facades\Route::is('dashboard') }}"
-            ></x-navigation.sidebar.item>
-            <x-navigation.sidebar.item-tree
-                icon="database"
-                title="Master"
-                :active="false"
-            >
-                <x-navigation.sidebar.item
-                    to="/master"
-                    icon="tags"
-                    title="Kategori"
-                    active="{{ \Illuminate\Support\Facades\Route::is('category') }}"
-                ></x-navigation.sidebar.item>
-                <x-navigation.sidebar.item
-                    to="/master"
-                    icon="box"
-                    title="Barang"
-                    active="{{ \Illuminate\Support\Facades\Route::is('item') }}"
-                ></x-navigation.sidebar.item>
-                <x-navigation.sidebar.item
-                    to="/master"
-                    icon="gift"
-                    title="Hadiah"
-                    active="{{ \Illuminate\Support\Facades\Route::is('gift') }}"
-                ></x-navigation.sidebar.item>
-                <x-navigation.sidebar.item
-                    to="/master"
-                    icon="route"
-                    title="Jalur"
-                    active="{{ \Illuminate\Support\Facades\Route::is('gift') }}"
-                ></x-navigation.sidebar.item>
-            </x-navigation.sidebar.item-tree>
-            <x-navigation.sidebar.item
-                to="/cashier"
-                icon="shopping-bag"
-                title="Kasir"
-            ></x-navigation.sidebar.item>
-            <x-navigation.sidebar.item
-                to="/point"
-                icon="arrow-left-right"
-                title="Penukaran Poin"
-            ></x-navigation.sidebar.item>
-            <x-navigation.sidebar.item
-                to="/users"
-                icon="users"
-                title="Pengguna"
-            ></x-navigation.sidebar.item>
-            <x-navigation.sidebar.item
-                to="/users"
-                icon="contact"
-                title="Member"
-            ></x-navigation.sidebar.item>
-            <x-navigation.sidebar.item
-                to="/users"
-                icon="calendar-check"
-                title="Jadwal"
-            ></x-navigation.sidebar.item>
-            <x-navigation.sidebar.item-tree
-                icon="clipboard-minus"
-                title="Laporans"
-                active="{{ \Illuminate\Support\Facades\Route::is('dashboard') }}"
-            >
-                <x-navigation.sidebar.item
-                    to="/users"
-                    icon="calendar-check"
-                    title="Jadwal"
-                ></x-navigation.sidebar.item>
-            </x-navigation.sidebar.item-tree>
-
+    <div class="w-full px-3">
+        <div class="w-full h-20 flex justify-center items-center border-b border-neutral-300 mb-3 ">
+            <img
+                src="{{ asset('/assets/images/logo.png') }}"
+                alt="img-logo"
+                class="h-12">
         </div>
     </div>
+    <div class="w-full flex-grow overflow-y-auto px-3 pb-3">
+        <div class="w-full">
+            <p class="text-xs text-neutral-400 font-light mb-1">Menu</p>
+            <div class="flex flex-col w-full gap-1">
+                {{ $slot }}
+            </div>
+        </div>
+    </div>
+    <!-- sidebar menu -->
+
     {{--    <div class="header">--}}
     {{--        @if(isset($header))--}}
     {{--        @endif--}}
