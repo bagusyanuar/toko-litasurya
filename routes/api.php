e40418b4e997fd702458ffd6e7a6cced8006c601
pradanna <pradanamahendra@gmail.com>
2025-01-07 14:27:11 +0700

    da

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware([JwtMiddleware::class])->group(function () {
     Route::get('/categories', [CategoryController::class, 'index']);
 
-    Route::middleware('auth:api')->get('/profile', [AuthController::class, 'getUserData'])->middleware('auth:api');
+    Route::get('/profile', [AuthController::class, 'getUserData']);
 
     Route::group(['prefix' => 'carts'], function () {
         Route::post('/', [CartController::class, 'store']);
