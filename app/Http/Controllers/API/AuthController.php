4f4cab75ac40797f08a9b9ae9d42e62694bcb700
pradanna <pradanamahendra@gmail.com>
2025-01-08 23:34:28 +0700

    tambahuser id di login

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -132,7 +132,8 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => JWTAuth::factory()->getTTL() * 60
+            'expires_in' => JWTAuth::factory()->getTTL() * 60,
+            'user_id' => JWTAuth::parseToken()->getPayload()->get('sub'),
         ]);
     }
 }
